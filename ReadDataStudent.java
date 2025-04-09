5d2bde4d7a1ab6a2d6bfe1bda4b2b560465820c0
Aryan-codes-ig <Aryan-codes-ig @ahs.local>
2025-04-09 10:03:19 -0700

    aaa

--- ReadDataStudent.java
+++ ReadDataStudent.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class ReadDataStudent{
     //I hard-coded the number of rows and columns so 
     //I could use a 2D array
-    private double[][] data = new double[29108][14];
+    private double[][] data = new double[21908][14];
 
     //This should read in the csv file and store the data in a 2D array,
     //data -- don't forget to skip the header line and parse everything
@@ -37,9 +37,9 @@ public class ReadDataStudent{
     //this should return a double array of the column
     //of data
     public double[] getColumn(int col){
-        double[] column = new double[data.length];
-        for (int r = 0; r < data.length; r++) {
-            column[r] = data[r][col];
+        double[] column = new double[21908];
+        for (int i = 0; i< column.length; i++){
+            column[i] = data[i][col];
         }
         return column;
     }
@@ -55,37 +55,36 @@ public class ReadDataStudent{
     public double stdDeviation(double[] arr){
         double sum = 0;
         double mean = mean(arr);
-        for(int i = 0; i < arr.length; i ++){
-            sum+= Math.pow((arr[i]-mean),2);
+        for (int i = 0; i < arr.length; i++){
+            sum += Math.pow(arr[i] - mean, 2);
         }
-        sum = sum/(arr.length-1);
-        
-        return Math.sqrt(sum) ;//sample variance!
+        sum = sum / (arr.length - 1); 
+        return Math.sqrt(sum); 
     }
     
     //this returns the mean of the column of data passed in
     //the mean is the sum of the values divided by the number of values
     public double mean(double[] arr){
-        double sum = 0.0;
-        for(int i = 0; i < arr.length; i ++){
-            sum  += arr[i];
+        double sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
         }
-        return (sum/arr.length);
+        return sum / arr.length;  
     }
 
     //this returns the values of a column in standard units
     //the standard units are the value minus the mean divided by the standard deviation
     //this should return a double array of the standard units
     public double[] standardUnits(double[] arr){
-        double[] stdArr = new double[arr.length];
+        double[] stdArr = new double[21908];
         double stdDeviation = stdDeviation(arr);
         double mean = mean(arr);
-        for(int i = 0; i < arr.length; i ++){
-           stdArr[i] = (arr[i] - mean) / stdDeviation;
+        for (int i = 0; i < arr.length; i++){
+            stdArr[i] = (arr[i] - mean) / stdDeviation;
         }
         return stdArr;
     }
-    
+
     //this returns the correlation between the two columns of data passed in
     //the correlation is the sum of the products of the standard units
     //of the two columns divided by the number of values - 1
